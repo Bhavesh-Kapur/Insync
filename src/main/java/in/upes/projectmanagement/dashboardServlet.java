c698ecbb5cf96ed7ccedfc9b4e07daf45004fd46
Bhavesh-Kapur <Bhavesh.105635@stu.upes.ac.in>
2024-09-23 08:37:52 +0530

    dashboard servlet mapping

---
+++ src/main/java/in/upes/projectmanagement/dashboardServlet.java
@@ -0,0 +1,21 @@
+package in.upes.projectmanagement;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class dashboardServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+        
+        if("teamCreation".equals(action)){
+            // response.sendRedirect(searchStudent.jsp);
+        }
+
+
+        //rest routing can be done here 
+    }
+}

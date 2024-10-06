7bc606e0cc3c813209fdd9e0e1ca45bb161d1848
Bhavesh-Kapur <Bhavesh.105635@stu.upes.ac.in>
2024-10-06 20:30:46 +0530

    decline invite servlet init

---
+++ src/main/java/in/upes/projectmanagement/DeclineInviteServlet.java
@@ -0,0 +1,44 @@
+package in.upes.projectmanagement;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+
+public class DeclineInviteServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        String inviteeSapid = (String) session.getAttribute("sapid"); // logged in user 
+        int sapid = Integer.parseInt(inviteeSapid);
+        String inviterName = request.getParameter("inviter_name"); // Inviter// Inviter's name
+
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/insync", "root", "rootbhavesh");
+            String query = "UPDATE invites SET inviteStatus = 'Declined' WHERE sapid = ? AND invitedBy = ?";
+            PreparedStatement stmt = connection.prepareStatement(query);
+            stmt.setInt(1, sapid);
+            stmt.setString(2, inviterName);
+            stmt.executeUpdate();
+            response.sendRedirect("");    //figure out the redirection for the page when the invite is declined 
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.sendRedirect("error.jsp");
+        } finally {
+            try {
+                if (connection != null) connection.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
